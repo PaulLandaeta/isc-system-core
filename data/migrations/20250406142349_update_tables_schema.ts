5fe11ae018655ef6a4671cd0dd4fc5f374b01a05
Sacha <santiagoacha10@.com>
2025-04-14 01:40:27 -0400

    Feat: Implementation of role permissions assignment and related updates

--- data/migrations/20250406142349_update_tables_schema.ts
+++ data/migrations/20250406142349_update_tables_schema.ts
@@ -1,8 +1,6 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-
-  // timestamps
   await knex.schema.alterTable('user_roles', (table) => {
     table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
@@ -29,21 +27,18 @@ export async function up(knex: Knex): Promise<void> {
     table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
   });
 
-  // permissions ordering to the table role_permissions
-  await knex.schema.alterTable('role_permissions', (table) =>{
+  await knex.schema.alterTable('role_permissions', (table) => { 
     table.integer('menu_order').notNullable();
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
   });
-
 }
 
 export async function down(knex: Knex): Promise<void> {
-
- // timestamps
- await knex.schema.alterTable('user_roles', (table) => {
+  await knex.schema.alterTable('user_roles', (table) => {
     table.dropColumn('created_at');
     table.dropColumn('updated_at');
   });
-
   await knex.schema.alterTable('knex_migrations', (table) => {
     table.dropColumn('created_at');
     table.dropColumn('updated_at');
@@ -52,24 +47,21 @@ export async function down(knex: Knex): Promise<void> {
     table.dropColumn('created_at');
     table.dropColumn('updated_at');
   });
-  
   await knex.schema.alterTable('students', (table) => {
-    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
-    table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+    table.dropColumn('created_at');
+    table.dropColumn('updated_at');
   });
-
   await knex.schema.alterTable('professors', (table) => {
-    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
-    table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+    table.dropColumn('created_at');
+    table.dropColumn('updated_at');
   });
-
   await knex.schema.alterTable('permission_categories', (table) => {
-    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
-    table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+    table.dropColumn('created_at');
+    table.dropColumn('updated_at');
   });
-
   await knex.schema.alterTable('role_permissions', (table) => {
     table.dropColumn('menu_order');
+    table.dropColumn('created_at');
+    table.dropColumn('updated_at');
   });
-
 }
