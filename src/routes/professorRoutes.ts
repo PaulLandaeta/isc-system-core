a1dd77c1ea76cae1516cdb9963c465c43840fe45
cosdealpha <bocangelcamila@gmail.com>
2025-04-14 11:49:59 -0400

    fix:rutaas tesistas del docente tutor

--- src/routes/professorRoutes.ts
+++ src/routes/professorRoutes.ts
@@ -3,10 +3,12 @@ import * as ProfessorController from '../controllers/professorController';
 import { checkUserAuth } from '../middlewares/checkUserAuth';
 import { validateBody } from '../middlewares/validateBodyMiddleware';
 import { professorSchema } from '../middlewares/schemas/createProfessorSchema';
+import { getThesisStudentsController } from '../controllers/tesistasDocenteTutorController';
 const router = express.Router();
 
 router.get('/', checkUserAuth, ProfessorController.getProfessorsController);
 router.post('/', checkUserAuth, validateBody(professorSchema), ProfessorController.createProfessor);
 router.get('/:id', checkUserAuth, ProfessorController.getProfessorById);
 router.delete('/:id', checkUserAuth, ProfessorController.deleteProfessorController);
+router.get('/thesis-students/:supervisorId', getThesisStudentsController);
 export default router;
