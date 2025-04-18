b4505525dc60ed9d271b6b02a0c1629fc6ae05e7
Angela Cuba <cpf.cuba13394732@gmail.com>
2025-04-17 20:21:53 -0400

    Graduation process module

--- data/seeds/04_graduation_process_module_seeds.ts
+++ data/seeds/04_graduation_process_module_seeds.ts
@@ -1,35 +1,28 @@
-
 import { Knex } from 'knex';
-
 
 const gradProcTable = 'graduation_process';
 const stagesTable = 'stages';
 const modalitiesTable = 'modalities';
 
-
 export const seed = async (knex: Knex): Promise<void> => {
- 
-  
   await knex(modalitiesTable).insert([
     {
       id: 1,
       name: 'Proyecto de Grado',
-      description: 'Modalidad Proyecto de Grado'
+      description: 'Modalidad Proyecto de Grado',
     },
     {
       id: 2,
       name: 'Trabajo Dirigido',
-      description: 'Modalidad Trabajo Dirigido'
+      description: 'Modalidad Trabajo Dirigido',
     },
     {
       id: 3,
       name: 'Tesis',
-      description: 'Modalidad Tesis'
-    }
+      description: 'Modalidad Tesis',
+    },
   ]);
 
-
-  
   await knex(stagesTable).insert([
     { id: 1, name: 'Inscripción a seminario' },
     { id: 2, name: 'Tutor' },
@@ -37,14 +30,11 @@ export const seed = async (knex: Knex): Promise<void> => {
     { id: 4, name: 'Revisión de documentos' },
     { id: 5, name: 'Defensa interna' },
     { id: 6, name: 'Revisión de documentos ministerio de educación' },
-    { id: 7, name: 'Defensa externa' }
+    { id: 7, name: 'Defensa externa' },
   ]);
 
-
-  
   await knex(gradProcTable).insert([
     {
-     
       student_id: 6,
       modality_id: 1,
       project_name: 'Sistema de Gestión de Biblioteca Digital',
@@ -52,140 +42,135 @@ export const seed = async (knex: Knex): Promise<void> => {
       date_seminar_enrollment: knex.fn.now(),
       period: '2025-1',
       tutor_letter: true,
-      tutor_id: 2, // Alexis Marechal
+      tutor_id: 2,
       tutor_approval: true,
       date_tutor_assignament: knex.fn.now(),
       reviewer_letter: true,
-      reviewer_id: 21, // Emma Silva
+      reviewer_id: 21,
       reviewer_approval: false,
-      stage_id: 2 // Tutor
+      stage_id: 2,
     },
     {
-     
-        student_id: 14, //nicolas gutierraz
-        modality_id: 1,
-        project_name: 'Sistema de Gestión de Biblioteca Digital',
-        seminar_enrollment: false,
-        date_seminar_enrollment: knex.fn.now(),
-        period: '2025-1',
-        tutor_letter: true,
-        tutor_id: 21, // emma
-        tutor_approval: true,
-        date_tutor_assignament: knex.fn.now(),
-        reviewer_letter: true,
-        reviewer_id: 2, // alexis
-        reviewer_approval: false,
-        stage_id: 2 // Tutor
-      },
+      student_id: 14,
+      modality_id: 1,
+      project_name: 'Sistema de Gestión de Biblioteca Digital',
+      seminar_enrollment: false,
+      date_seminar_enrollment: knex.fn.now(),
+      period: '2025-1',
+      tutor_letter: true,
+      tutor_id: 21,
+      tutor_approval: true,
+      date_tutor_assignament: knex.fn.now(),
+      reviewer_letter: true,
+      reviewer_id: 2,
+      reviewer_approval: false,
+      stage_id: 2,
+    },
     {
-      
-      student_id: 7, // Mariana Del Arroyo
+      student_id: 7,
       modality_id: 2,
       project_name: 'Plataforma de Aprendizaje Automatizado',
       seminar_enrollment: true,
       date_seminar_enrollment: knex.fn.now(),
       period: '2025-2',
       tutor_letter: true,
-      tutor_id: 22, // Tomás Muñoz
+      tutor_id: 22,
       tutor_approval: true,
       date_tutor_assignament: knex.fn.now(),
       reviewer_letter: false,
-      reviewer_id: 21, // Emma Silva
+      reviewer_id: 21,
       reviewer_approval: false,
-      stage_id: 2 // Revisor
-      
+      stage_id: 2,
     },
     {
-      //tesis
-     
-      student_id: 8, // Camilo Zuleta
+      student_id: 8,
       modality_id: 3,
       project_name: 'Análisis de Algoritmos de Machine Learning',
       seminar_enrollment: true,
       date_seminar_enrollment: knex.fn.now(),
       period: '2026-1',
       tutor_letter: true,
-      tutor_id: 23, // Isabela Cáceres
+      tutor_id: 23,
       date_tutor_assignament: knex.fn.now(),
       reviewer_letter: false,
-      reviewer_id: 22, //tomas
+      reviewer_id: 22,
       reviewer_approval: false,
-      stage_id: 5 // Defensa interna
+      stage_id: 5,
     },
     {
-      student_id: 9, // María López
+      student_id: 9,
       modality_id: 2,
       project_name: 'Sistema de Gestión de Horarios',
       seminar_enrollment: true,
       date_seminar_enrollment: knex.fn.now(),
       period: '2026-1',
       tutor_letter: true,
-      tutor_id: 24, // Francisco Ortega
+      tutor_id: 24,
       date_tutor_assignament: knex.fn.now(),
       reviewer_letter: false,
-      reviewer_id: 22, //tomas
+      reviewer_id: 22,
       reviewer_approval: false,
-      stage_id: 2 // Tutor
+      stage_id: 2,
     },
     {
-      student_id: 10, // Javier Martínez
+      student_id: 10,
       modality_id: 1,
       project_name: 'Aplicación Móvil para Registro Académico',
       seminar_enrollment: false,
       date_seminar_enrollment: knex.fn.now(),
       period: '2025-2',
       tutor_letter: false,
-      tutor_id: 2, // Alexis Marechal
+      tutor_id: 2,
       date_tutor_assignament: knex.fn.now(),
       reviewer_letter: false,
-      reviewer_id: 24, //francisco
+      reviewer_id: 24,
       reviewer_approval: false,
-      stage_id: 4 // Revisión de documentos
+      stage_id: 4,
     },
     {
-      student_id: 11, // Valentina Pérez
+      student_id: 11,
       modality_id: 3,
       project_name: 'Framework para Análisis de Datos Educativos',
       seminar_enrollment: true,
       date_seminar_enrollment: knex.fn.now(),
       period: '2025-1',
       tutor_letter: true,
-      tutor_id: 21, // Emma Silva
+      tutor_id: 21,
       date_tutor_assignament: knex.fn.now(),
       reviewer_letter: false,
-      reviewer_id: 23, //isabela
+      reviewer_id: 23,
       reviewer_approval: false,
-      stage_id: 6 // Revisión ministerio
+      stage_id: 6,
     },
     {
-      student_id: 12, // Andrés Zamora
+      student_id: 12,
       modality_id: 3,
       project_name: 'Plataforma de Colaboración Estudiantil',
       seminar_enrollment: true,
       date_seminar_enrollment: knex.fn.now(),
       period: '2025-2',
       tutor_letter: true,
-      tutor_id: 22, // Tomás Muñoz
+      tutor_id: 22,
       date_tutor_assignament: knex.fn.now(),
       reviewer_letter: true,
-      reviewer_id: 2, //alexis
+      reviewer_id: 2,
       reviewer_approval: false,
-      stage_id: 2 // Tutor
+      stage_id: 2,
     },
     {
-      student_id: 13, // Cristina Fernández
+      student_id: 13,
       modality_id: 2,
       project_name: 'Sistema de Recomendación de Cursos',
       seminar_enrollment: true,
       date_seminar_enrollment: knex.fn.now(),
       period: '2025-1',
       tutor_letter: true,
-      tutor_id: 23, // Isabela Cáceres
+      tutor_id: 23,
       date_tutor_assignament: knex.fn.now(),
       reviewer_letter: true,
-      reviewer_id: 22, //tomas
+      reviewer_id: 22,
       reviewer_approval: true,
-      stage_id: 5 // Defensa interna
-    }
+      stage_id: 5,
+    },
   ]);
 };
