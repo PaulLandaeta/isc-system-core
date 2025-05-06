d0bf3ec8be3d4b4b87f546ea404befe27712d0f4
CamiloRaphaelZuletaWolff <camiloraphaelzuletawolff@gmail.com>
2025-05-05 21:21:46 -0400

    Finalization of the seeds modularization

--- data/seeds/03_user_module_seeds.ts
+++ data/seeds/03_user_module_seeds.ts
@@ -4,6 +4,7 @@ const userProfileTable = 'user_profile';
 const internsTable = 'interns';
 const professorTable = 'professors';
 const studentsTable = 'students';
+const userRolesTable = 'user_roles';
 
 
 export const seed = async (knex: Knex): Promise<void> => {
@@ -385,4 +386,31 @@ export const seed = async (knex: Knex): Promise<void> => {
       specialty: 'Cybersecurity',
     },
   ]);
+  await knex(userRolesTable).insert([
+    { user_id: 1, role_id: 1 },
+    { user_id: 2, role_id: 2 },
+    { user_id: 21, role_id: 2 },
+    { user_id: 22, role_id: 2 },
+    { user_id: 23, role_id: 2 },
+    { user_id: 24, role_id: 2 },
+    { user_id: 6, role_id: 3 },
+    { user_id: 7, role_id: 3 },
+    { user_id: 8, role_id: 3 },
+    { user_id: 9, role_id: 3 },
+    { user_id: 10, role_id: 3 },
+    { user_id: 11, role_id: 3 },
+    { user_id: 12, role_id: 3 },
+    { user_id: 13, role_id: 3 },
+    { user_id: 14, role_id: 3 },
+    { user_id: 15, role_id: 3 },
+    { user_id: 3, role_id: 4 },
+    { user_id: 16, role_id: 4 },
+    { user_id: 17, role_id: 4 },
+    { user_id: 18, role_id: 4 },
+    { user_id: 19, role_id: 4 },
+    { user_id: 20, role_id: 4 },
+    { user_id: 4, role_id: 5 },
+    { user_id: 5, role_id: 6 }
+  ]);
+  
 };
