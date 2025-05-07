68da0bc0f8747c7b58ff3f04dbf8d374f3b30092
theTarnished <alex4750384@gmail.com>
2025-05-07 03:40:45 -0400

    add a new column (disabled:boolean) to professors

--- data/migrations/20240903031745_update_user_profile.ts
+++ data/migrations/20240903031745_update_user_profile.ts
@@ -2,6 +2,7 @@ import type { Knex } from 'knex';
 
 const userProfileTable = 'user_profile';
 const studentTable = 'students';
+const professorTable = 'professors'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable(userProfileTable, table => {
@@ -10,6 +11,10 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable(studentTable, table => {
     table.dropColumn('code');
   });
+  await knex.schema.alterTable(professorTable, table => {
+    table.boolean('disabled').notNullable().defaultTo(false);
+  });
+
 }
 
 export async function down(knex: Knex): Promise<void> {
@@ -18,5 +23,8 @@ export async function down(knex: Knex): Promise<void> {
   });
   await knex.schema.alterTable(studentTable, table => {
     table.string('code').notNullable();
+  });
+  await knex.schema.alterTable(professorTable, table => {
+    table.dropColumn('disabled');
   });
 }
