214e5564e34292e68e903680c1980556e81ef4bd
theTarnished <alex4750384@gmail.com>
2025-06-05 21:13:01 -0400

    fix: fix merge conflicts

--- src/services/rolesService.ts
+++ src/services/rolesService.ts
@@ -73,7 +73,7 @@ export const disableRol = async (id: number) => {
   }
 };
 
-export const addPermission = async (ides: rolePermissionsRequest) => {
+export const addPermission = async (ides: rolePermissionsRequest[]) => {
   try {
     const rolePermission = await RolesRepository.addPermission(ides);
     return rolePermission;
